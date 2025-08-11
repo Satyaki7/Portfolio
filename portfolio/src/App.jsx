30920c51c999a3aeeacdd51d9d1f125ea427faf1
Satyaki7 <dsatyaki956@gmail.com>
2025-08-11 14:03:41 +0530

    Initial Commit

---
+++ portfolio/src/App.jsx
@@ -0,0 +1,11 @@
+import Testing from "./components/testing"
+
+function App() {
+  return (
+    <>
+      <Testing/>
+    </>
+  )
+}
+
+export default App
---
+++ portfolio/src/components/Testing.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Testing = () => {
+  return (
+    <div className="flex items-center justify-center min-h-screen">
+      <div className="bg-red-500 w-64 h-64 flex items-center justify-center rounded-lg">
+        {/* Content goes here */}
+      </div>
+    </div>
+  );
+};
+
+export default Testing;
